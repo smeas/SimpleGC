316a055ccedd258a5eceaff2efa406e95d83eed3
Jonatan Johansson <smeas@users.noreply.github.com>
2025-02-07 00:27:18 +0100

    swap vector with unordered_set

--- gc.cpp
+++ gc.cpp
@@ -2,17 +2,17 @@
 
 #include <stdlib.h>
 #include <vector>
+#include <unordered_set>
 #include <algorithm>
 #include <mutex>
 #include <thread>
 #include <Windows.h>
 
-// bucket 8..64
-
 struct gc_state
 {
 	std::vector<void*> roots;
-	std::vector<void*> objects;
+	std::unordered_set<void*> objects;
+	std::vector<void*> toFree;
 };
 
 
@@ -21,6 +21,7 @@ struct gc_header
 	size_t size;
 	int mark;
 };
+static_assert(sizeof(gc_header) % 8 == 0);
 
 #define OBJ_HEADER(obj_ptr) ((struct gc_header*)(obj_ptr) - 1)
 #define LIST_CONTAINS(list, elem) (std::find(std::begin(list), std::end(list), (elem)) != std::end(list))
@@ -32,10 +33,6 @@ void gc_add_root(struct gc_state* gc, void* obj)
 
 gc_state* gc_create()
 {
-	// TODO: Change after deleting C++...
-	//gc_state* gc = (gc_state*)malloc(sizeof(gc_state));
-	//memset(gc, 0, sizeof(gc_state));
-
 	return new gc_state;
 }
 
@@ -47,15 +44,16 @@ size_t gc_get_object_count(gc_state* gc)
 void* gc_new(struct gc_state* gc, size_t size)
 {
 	size = (size + 7) & ~7;
-	void* alloc = malloc(sizeof(gc_header) + size);
+	gc_header* alloc = (gc_header*)malloc(sizeof(gc_header) + size);
 	if (!alloc) return NULL;
-	gc_header* header = (gc_header*)alloc;
-	*header = gc_header {};
-	header->size = size;
-	header->mark = 1;
-	void* obj = (char*)alloc + sizeof(gc_header);
-	gc->objects.push_back(obj);
-	return obj;
+
+	//*alloc = gc_header{};
+	alloc->size = size;
+	alloc->mark = 1;
+
+	void* objPtr = (alloc + 1);
+	gc->objects.insert(objPtr);
+	return objPtr;
 }
 
 void* gc_new0(gc_state* gc, size_t size)
@@ -67,32 +65,22 @@ void* gc_new0(gc_state* gc, size_t size)
 
 void gc_free(gc_state* gc, void* obj)
 {
-	for (size_t i = 0; i < gc->objects.size(); i++)
-	{
-		if (gc->objects[i] == obj)
-		{
-			gc->objects.erase(gc->objects.begin() + i);
-			free((char*)obj - sizeof(gc_header));
-			return;
-		}
-	}
+	if (gc->objects.erase(obj))
+		free(OBJ_HEADER(obj));
 }
 
-void gc_mark_root(gc_state* gc, void* root)
+void gc_mark_recursive(gc_state* gc, void* obj)
 {
-	OBJ_HEADER(root)->mark = 1;
-	size_t size = OBJ_HEADER(root)->size;
+	OBJ_HEADER(obj)->mark = 1;
+	size_t size = OBJ_HEADER(obj)->size;
 	for (size_t i = 0; i < size / 8; i++)
 	{
 		//void* testPtr = ((uintptr_t*)root + i);
-		void* testPtr = (((uintptr_t**)root)[i]);
-		// The list contain check could be optimized if we knew the bounds of the gc heap.
-		if (LIST_CONTAINS(gc->objects, testPtr) && !OBJ_HEADER(testPtr)->mark)
-		{
-			// Value is an unmarked obj ptr.
-			//OBJ_HEADER(testPtr)->mark = 1;
-			gc_mark_root(gc, testPtr);
-		}
+		void* testPtr = ((uintptr_t**)obj)[i];
+
+		// TODO: The list contain check could be optimized if we knew the bounds of the gc heap.
+		if (gc->objects.count(testPtr) && !OBJ_HEADER(testPtr)->mark)
+			gc_mark_recursive(gc, testPtr);
 	}
 }
 
@@ -105,18 +93,19 @@ void gc_collect(gc_state* gc)
 		OBJ_HEADER(obj)->mark = 0;
 
 	for (void* root : gc->roots)
-		gc_mark_root(gc, root);
+		gc_mark_recursive(gc, root);
 
-	size_t i = gc->objects.size() - 1;
-	while (1)
+	for (void* obj : gc->objects)
 	{
-		if (!OBJ_HEADER(gc->objects[i])->mark)
-		{
-			free(OBJ_HEADER(gc->objects[i]));
-			gc->objects.erase(gc->objects.begin() + i);
-		}
-
-		if (i == 0) break;
-		i--;
+		if (!OBJ_HEADER(obj)->mark)
+			gc->toFree.push_back(obj);
 	}
+
+	for (void* obj : gc->toFree)
+	{
+		gc->objects.erase(obj);
+		free(OBJ_HEADER(obj));
+	}
+
+	gc->toFree.clear();
 }
