0e8bf5e0ffd11759bf5a945a5837fddfda49fa3f
Jonatan Johansson <smeas@users.noreply.github.com>
2025-02-07 01:06:22 +0100

    pack header data

--- gc.cpp
+++ gc.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <unordered_set>
 #include <algorithm>
+#include <assert.h>
 #include <mutex>
 #include <thread>
 #include <Windows.h>
@@ -15,14 +16,20 @@ struct gc_state
 	std::vector<void*> toFree;
 };
 
-
 struct gc_header
 {
-	size_t size;
-	int mark;
+	// 1. most significant bit stores the mark.
+	// 2. the rest of the value stores the size of the allocated object, excluding the header.
+	size_t value;
 };
 
+constexpr size_t HI_BIT64 = (size_t)1 << 63;
+
 #define OBJ_HEADER(obj_ptr) ((struct gc_header*)(obj_ptr) - 1)
+#define OBJ_GET_SIZE(obj_ptr) (OBJ_HEADER(obj_ptr)->value & ~HI_BIT64)
+#define OBJ_GET_MARKED(obj_ptr) (OBJ_HEADER(obj_ptr)->value > (HI_BIT64 - 1))
+#define OBJ_SET_MARKED(obj_ptr) (OBJ_HEADER(obj_ptr)->value |= HI_BIT64)
+#define OBJ_SET_UNMARKED(obj_ptr) (OBJ_HEADER(obj_ptr)->value &= ~HI_BIT64)
 
 void gc_add_root(struct gc_state* gc, void* obj)
 {
@@ -41,14 +48,14 @@ size_t gc_get_object_count(gc_state* gc)
 
 void* gc_new(struct gc_state* gc, size_t size)
 {
-	//size = (size + 7) & ~7;
 	static_assert(sizeof(gc_header) % 8 == 0, "");
+	assert(size < HI_BIT64);
+
+	//size = (size + 7) & ~7;
 	gc_header* alloc = (gc_header*)malloc(sizeof(gc_header) + size);
 	if (!alloc) return NULL;
 
-	//*alloc = gc_header{};
-	alloc->size = size;
-	alloc->mark = 0;
+	alloc->value = size;
 
 	void* objPtr = (alloc + 1);
 	gc->objects.insert(objPtr);
@@ -70,8 +77,8 @@ void gc_free(gc_state* gc, void* obj)
 
 void gc_mark_recursive(gc_state* gc, void* obj)
 {
-	OBJ_HEADER(obj)->mark = 1;
-	size_t size = OBJ_HEADER(obj)->size;
+	OBJ_SET_MARKED(obj);
+	size_t size = OBJ_GET_SIZE(obj);
 	static_assert(sizeof(uintptr_t) == 8, "");
 
 	for (size_t i = 0; i < size / 8; i++)
@@ -79,7 +86,7 @@ void gc_mark_recursive(gc_state* gc, void* obj)
 		void* testPtr = ((uintptr_t**)obj)[i];
 
 		// TODO: The list contain check could be optimized if we knew the bounds of the gc heap.
-		if (gc->objects.count(testPtr) && !OBJ_HEADER(testPtr)->mark)
+		if (gc->objects.count(testPtr) && !OBJ_GET_MARKED(testPtr))
 			gc_mark_recursive(gc, testPtr);
 	}
 }
@@ -91,10 +98,10 @@ void gc_collect(gc_state* gc)
 
 	for (void* obj : gc->objects)
 	{
-		if (!OBJ_HEADER(obj)->mark)
+		if (!OBJ_GET_MARKED(obj))
 			gc->toFree.push_back(obj);
 
-		OBJ_HEADER(obj)->mark = 0;
+		OBJ_SET_UNMARKED(obj);
 	}
 
 	for (void* obj : gc->toFree)
