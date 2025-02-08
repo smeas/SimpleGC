9155d465dca6b391d7e20de50444fd3d78d31314
Jonatan Johansson <smeas@users.noreply.github.com>
2025-02-08 04:28:54 +0100

    rename project

--- gc.h
+++ gc.h
@@ -7,6 +7,5 @@ void* gc_new0(struct gc_state* gc, size_t size);
 void gc_free(struct gc_state* gc, void* obj);
 void gc_collect(struct gc_state* gc);
 void gc_add_root(struct gc_state* gc, void* obj);
-gc_state* gc_create();
-
 size_t gc_get_object_count(struct gc_state* gc);
+gc_state* gc_create();
